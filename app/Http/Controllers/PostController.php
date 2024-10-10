2967599afbab772604cd15feb30c1c0b5bc52e04
akiko04m <amkoli2@docomo.ne.jp>
2024-10-10 16:08:51 +0900

    lev7-2-2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,11 +7,10 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(Post $post)
-    {
-        return view('posts.index')->with(['posts' => $post->get()]);
-       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
-    }
+public function index(Post $post)
+{
+    return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);//getPaginateByLimit()はPost.phpで定義したメソッドです。
+}
 }
 
 
